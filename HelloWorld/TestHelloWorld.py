dd0ea92901e20fcb33ff2a0bfc288a1e8e7de757
Alex Araujo <Alex.Araujo@nielsen.com>
2018-04-17 14:07:28 -0400

    Hello World commit

---
+++ HelloWorld/TestHelloWorld.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+from io import StringIO
+import HelloWorld
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_TestHelloWorld(self):
+        out = StringIO()
+        sys.stdout = out
+        HelloWorld.HelloWorld()
+        self.assertEqual(out.getvalue().strip(), "Hello World")
+
+if __name__ == '__main__':
+    
+    unittest.main(verbosity = 2)
